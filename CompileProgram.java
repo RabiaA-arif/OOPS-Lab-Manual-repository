d21740f81b665c4dc437bf4c5f493092039214a7
Rabia Arif <151766471+RabiaA-arif@users.noreply.github.com>
2024-05-12 19:29:51 +0500

    lab manual practical 1 exercise question

---
+++ CompileProgram.java
@@ -0,0 +1,18 @@
+package LabManul;
+
+import java.util.Scanner;
+
+public class CompileProgram {
+    public static void main(String[] args) {
+        System.out.println("Step 1: Write your Java code in a text editor or an IDE.");
+        System.out.println("Step 2: Save the file with a .java extension, for example, CompileProgram.java.");
+        System.out.println("Step 3: Open a terminal .");
+        System.out.println("Step 4: Navigate to the directory containing your Java file using the 'cd' command.");
+        System.out.println("Step 5: Compile your Java code using the 'javac' command followed by the filename with the .java extension.");
+        System.out.println("        Example: javac MyProgram.java");
+        System.out.println("Step 6: If there are no compilation errors, the compiler will generate a .class file.");
+        System.out.println("Step 7: To execute the compiled program, use the 'java' command followed by the name of the class containing the main method.");
+        System.out.println("        Example: java MyProgram");
+    }
+
+    }
---
+++ UserName.java
@@ -0,0 +1,12 @@
+package LabManul;
+
+import java.util.Scanner;
+
+public class UserName {
+    public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("give your name");
+        String username=input.nextLine();
+        System.out.println(username);
+    }
+}
